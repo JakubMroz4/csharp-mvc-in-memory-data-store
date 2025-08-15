b0fe571d2cd11357cf14522fec03a95f41d0eb44
Jakub Mroz <kuba.mroz01@gmail.com>
2025-08-15 15:28:36 +0200

    fix: model.Price int

--- exercise.wwwapi/DTOs/ProductPost.cs
+++ exercise.wwwapi/DTOs/ProductPost.cs
@@ -4,6 +4,6 @@
     {
         public string Name { get; set; }
         public string Category { get; set; }
-        public decimal Price { get; set; }
+        public int Price { get; set; }
     }
 }
--- exercise.wwwapi/Models/Product.cs
+++ exercise.wwwapi/Models/Product.cs
@@ -8,6 +8,6 @@ namespace exercise.wwwapi.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Category { get; set; }
-        public decimal Price { get; set; }
+        public int Price { get; set; }
     }
 }
